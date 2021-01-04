2bf83657dcb7b7236ae0c6c782abc47c999c3a2a
William J. Bowman <wjb@williamjbowman.com>
2021-01-04 14:18:07 -0800

    Add missing dependencies

--- cpsc411-interrogator-lib/info.rkt
+++ cpsc411-interrogator-lib/info.rkt
@@ -7,7 +7,11 @@
     "web-server-lib"
     ("base" #:version "7.4")
     "cpsc411-lib"
-    "cpsc411-reference-lib"))
+    "cpsc411-reference-lib"
+    "functional-lib"
+    "racket-index"
+    "scribble-lib"
+    "scribble-bettergrammar-lib"))
 (define pkg-desc "implementation (no documentation, tests) of the cpsc411 interrogator")
 (define version "0.1")
 (define pkg-authors '(wilbowma))
