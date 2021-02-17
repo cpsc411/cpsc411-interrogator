f95ad56c2cf7de35c212b8eae93f84afad37738c
William J. Bowman <wjb@williamjbowman.com>
2021-02-17 01:06:00 -0800

    Update a6-sanbox valid-id-set

--- cpsc411-interrogator-lib/cpsc411/interrogator/a6-sandbox.rkt
+++ cpsc411-interrogator-lib/cpsc411/interrogator/a6-sandbox.rkt
@@ -51,29 +51,36 @@
 (define valid-id-set
   (append
    '(check-values-lang
-     type-check-values-lang
      uniquify
+     sequentialize-let
+     impose-calling-conventions
+     canonicalize-bind
      select-instructions
      uncover-locals
      undead-analysis
      conflict-analysis
-     pre-assign-frame-variables
-     assign-frames
+     assign-call-undead-variables
+     allocate-frames
      assign-registers
-     assign-frame-variables
-     discard-call-live
      replace-locations
+     assign-frame-variables
      implement-fvars
+     optimize-predicates
      expose-basic-blocks
+     resolve-predicates
      flatten-program
      patch-instructions
      generate-x64
-     interp-paren-x64
-     link-paren-x64
+
      current-assignable-registers
      current-patch-instructions-registers
      current-parameter-registers
      current-return-value-register
      current-return-address-register
-     current-frame-base-pointer-register)
+     current-frame-base-pointer-register
+
+     interp-paren-x64
+     link-paren-x64
+
+     compile)
    base-valid-id-set))
